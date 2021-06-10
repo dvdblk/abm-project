279b70babb3cbbd82aa30be31aa50eab5871a885
bmcode86 <menz.brandy@gmail.com>
2021-06-11 00:09:26 +0200

    add a new plot function vola_alpha_scatterplot
    The function makes scatter plot for alpha and vola, also save the plot into 'out' folder

--- lib/plots.py
+++ lib/plots.py
@@ -13,3 +13,30 @@ def default_plot(times, attendances):
     ax.set_ylabel("A(t)")
 
     plt.show()
+
+def vola_alpha_scatterplot(alphas, vola, n_agents, s, labels=None, pout=None):
+    """
+    For giving lists alphas and vola, the function makes scatter plot
+    when pout = True, the plot can be saved locally into 'out' folder
+    """
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.axhline(y=1, color="k", linestyle="--") # baseline: vol = 1 -> randomness 
+    ax.plot(alphas, vola)
+    ax.set_xlabel(r"$\alpha = \frac{2^m}{N}$",fontsize=15)
+    ax.set_ylabel(r"$\frac{\sigma^2}{N}$", fontsize=15)
+    if labels:
+        ax.legend(labels)
+
+    plt.title("Volatilty as a function of alpha \n (MG with s=%s, N=%s)"%(s, n_agents), fontsize=18)
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.xlim([0.01,100])
+    plt.ylim([0.1,100])
+
+    if pout:
+        plt.savefig('out/%s_%s_vola_p.svg' % (n_agents, s))
+
+
+
+
+
